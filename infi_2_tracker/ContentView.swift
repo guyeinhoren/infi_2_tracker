c400f4ccfcfd8452b736b5a4f1ef50d196c6578c
guyeinhoren <guyeinhoren@gmail.com>
2026-08-27 09:57:02 +0300

    Initial Commit

---
+++ infi_2_tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  infi_2_tracker
+//
+//  Created by גיא אינהורן on 27/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ infi_2_tracker/infi_2_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  infi_2_trackerApp.swift
+//  infi_2_tracker
+//
+//  Created by גיא אינהורן on 27/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct infi_2_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
